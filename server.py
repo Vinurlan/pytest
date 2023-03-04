8e1c3a9c8af976f11cb69b86d192aa8e869ad9bc
Denis Danshin <dosug.d@gmail.com>
2023-03-05 01:35:36 +0300

    test

--- server.py
+++ server.py
@@ -5,9 +5,6 @@ from classes.Quote import Quote
 app = Flask(__name__)
 api = Api(app)
 
-app.config['ENV'] = 'prodaction'
-app.config['APP'] = 'prodaction'
-
 api.add_resource(Quote, '/ai-quotes', '/ai-quotes/', '/ai-quotes/<int:id>')
 if __name__ == '__main__':
     app.run(debug=True, host='localhost', port='5000')
