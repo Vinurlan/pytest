4ff46a0fd7d58d4e4c32c3d635fd110e3b06df31
Denis Danshin <dosug.d@gmail.com>
2023-03-13 22:05:27 +0300

    test

--- application.py
+++ application.py
@@ -2,6 +2,6 @@ from flask import Flask
 
 app = Flask(__name__)
 # app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:root@localhost/flasksql'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://tester:tester@localhost:5432/tester'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://tester:root@localhost:5432/tester'
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 app.secret_key = 'hi'
